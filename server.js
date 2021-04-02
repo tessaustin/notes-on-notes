1b8bde4ad04e4f2dc6a75626c311cf6a713f7199
Tess Austin <tessaustin92@gmail.com>
2021-04-02 12:52:33 -0600

    insomnia GET and POST seem to be working

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const { noteData } = require('./db/db.json')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
 app.use(express.json());
 
 //////FUNCTIONS//////
