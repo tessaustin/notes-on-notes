264c3287d0c533ce2ee4a3d0fac83b4236057fef
Tess Austin <tessaustin92@gmail.com>
2021-04-02 13:03:10 -0600

    Front-end html, css applied

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const { noteData } = require('./db/db.json')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+//////MIDDLEWARE//////
+
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
 
 //////FUNCTIONS//////
 
@@ -49,6 +54,7 @@ function validateNoteData(data) {
     return true;
 }
 
+
 //////ROUTES//////
 
 app.get('/api/notes', (req, res) => {
@@ -77,6 +83,14 @@ app.post('/api/notes', (req, res) => {
         const newNote = createNewNote(req.body, noteData);
         res.json(newNote);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
 app.listen(PORT, () => {
